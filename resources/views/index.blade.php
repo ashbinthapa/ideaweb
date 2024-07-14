8f2e6786a383eb60b880fd86c552f0b208e7d647
unknown <ashbinthapa2054@gmail.com>
2024-07-14 13:18:31 -0700

    learning how to include, extends and many more

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/index.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the content of the home page.</p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/partials/header.blade.php -->
+<nav>
+    <ul>
+        <li><a href="{{ url('/') }}">Home</a></li>
+        <li><a href="{{ url('/about') }}">About</a></li>
+        <li><a href="{{ url('/contact') }}">Contact</a></li>
+    </ul>
+</nav>

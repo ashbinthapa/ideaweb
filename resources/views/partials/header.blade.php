4a9a96051192a9c3b0d7f8514bcd74a0204881c6
unknown <ashbinthapa2054@gmail.com>
2024-07-19 13:00:53 -0700

    navigation bar bootstrap

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,7 @@
 <!-- resources/views/partials/header.blade.php -->
-<div class="container-ashbin">
-    <p>{{ $formattedDate }}</p>
-    <p>
-        Latest:
-</p>
+<div class="container">
+    {{ $formattedDate }}<br>
+    Latest:
     <div class="ticker-container">
         <div class="ticker-item">Apple in a Basket</div>
         <div class="ticker-item">Boy at the Park</div>
@@ -11,28 +9,63 @@
         <div class="ticker-item">Dog in the Kennel</div>
         <div class="ticker-item">Eggs in the Bowl</div>
     </div>
+
     <div class="header-facebook-icon">
         <a href="https://www.facebook.com/isernepalofficial/?ref=aymt_homepage_panel&eid=ARCZ8VyOFfG6ZEJl7hyyqg7-0wlTGyxtRQQwQU2psMbjH59aUSY-HQ5o-YnV3JiVR1NXkPDdzVzzR-06">
             <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
         </a>
     </div>
-
 </div>
+
 <div class="website-header-main-title">
     <p>
-	Improving Learning: Developing Measures of Accountability and Evaluating their Association with Student’s Gains in Achievement in Nepal
-	</p>
+        Improving Learning: Developing Measures of Accountability and Evaluating their Association with Student’s Gains in Achievement in Nepal
+    </p>
 </div>
+
 <div class="header-image-container">
     <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser">
 </div>
 
-
+<div class="container-fluid">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
 </div>
-<nav>
-    <ul>
-        <li><a href="{{ url('/') }}">Home</a></li>
-        <li><a href="{{ url('/about') }}">About</a></li>
-        <li><a href="{{ url('/contact') }}">Contact</a></li>
-    </ul>
-</nav>

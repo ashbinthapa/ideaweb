d6b24b217c364804755df5d4f88509471e34549e
unknown <ashbinthapa2054@gmail.com>
2024-07-21 15:31:07 -0700

    backgroung color changed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -27,9 +27,9 @@
     <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser">
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid website-header-main-title">
     <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

f67ddd92ef8a43ad5e6418622d4f481aa763de4f
unknown <ashbinthapa2054@gmail.com>
2024-07-28 16:01:32 -0700

    update:widget title design 2

--- resources/views/partials/body.blade.php
+++ resources/views/partials/body.blade.php
@@ -1,4 +1,25 @@
 <!-- resources/views/partials/body.blade.php -->
-<div class="container">
+<div class="container ashbin-a-tag ashbin-widget-image">
     <h3 class="ashbin-widget-title"><span>Recent Activities</span></h3>
+    <ul>
+        <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+    </ul>
+
+    <h3 class="ashbin-widget-title"><span>Important Links</span></h3>
+    <ul>
+        <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+        <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+    </ul>
+
+    <h3 class="ashbin-widget-title"><span>Images and Videos</span></h3>
+    <div><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+    <div><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+    <div><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
 </div>

72cfe99478e988ba8282051ed151f36e16f9afe3
unknown <ashbinthapa2054@gmail.com>
2024-07-28 14:00:44 -0700

    widget title design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,14 @@
     </header>
 
     <div class="container">
-        @yield('content')
+        <div class="row">
+            <div class="col-8">
+                @yield('content')
+            </div>
+            <div class="col-4">
+                @include('partials.body')
+            </div>
+        </div>
     </div>
 
     <footer>
---
+++ resources/views/partials/body.blade.php
@@ -0,0 +1,4 @@
+<!-- resources/views/partials/body.blade.php -->
+<div class="container">
+    <h3 class="widget-title"><span>Recent Activities</span></h3>
+</div>

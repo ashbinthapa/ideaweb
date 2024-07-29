47c57f428851715aaec1a4b18e5a843dcfbce9a9
unknown <ashbinthapa2054@gmail.com>
2024-07-28 17:00:54 -0700

    update: footer design image

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,4 +5,10 @@
             Improving Learning: Developing Measures of Accountability and Evaluating their Association with Student’s Gains in Achievement in Nepal
         </p>
     </div>
+    <div class="row ashbin-footer-img">
+        <div class="col-lg-3 col-6"><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+        <div class="col-lg-3 col-6"><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+        <div class="col-lg-3 col-6"><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+        <div class="col-lg-3 col-6"><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
+    </div>
 </div>

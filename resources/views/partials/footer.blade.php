b6ae27baa306896ebe24976aa4220889509de61f
unknown <ashbinthapa2054@gmail.com>
2024-07-29 12:57:39 -0700

    main footer design updated

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,3 +12,50 @@
         <div class="col-lg-3 col-6"><a href="#"> <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser"></a></div>
     </div>
 </div>
+<div class="container-fluid ashbin-footer-widgets-wrapper">
+    <div class="container ashbin-a-tag website-footer-content">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xxl-3">
+                <h3 class="ashbin-footer-widget-title"><span>Recent Activities</span></h3>
+                <ul>
+                    <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xxl-3">
+                <h3 class="ashbin-footer-widget-title"><span>Recent Activities</span></h3>
+                <ul>
+                    <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xxl-3">
+                <h3 class="ashbin-footer-widget-title"><span>Recent Activities</span></h3>
+                <ul>
+                    <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xxl-3">
+                <h3 class="ashbin-footer-widget-title"><span>Recent Activities</span></h3>
+                <ul>
+                    <li><a href="#"> DISSEMINATION WORKSHOP FOR IMPROVING LEARNING: DEVELOPING MEASURES OF ACCOUNTABILITY AND EVALUATING THEIR ASSOCIATION WITH STUDENTS’ GAINS IN ACHIEVEMENT IN NEPAL</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                    <li><a href="#"> OBSERVATION TRAINING FOR OBSERVER</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>

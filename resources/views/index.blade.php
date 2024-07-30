edd474cf236b3f7d63ccc0ceab41e47cb6bd380e
unknown <ashbinthapa2054@gmail.com>
2024-07-30 11:53:58 -0700

    index page design updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,31 @@
 @section('title', 'IDEA - Improving Learning: Developing Measures of Accountability and Evaluating their Association with Student’s Gains in Achievement in Nepal')
 
 @section('content')
-    <h1>Welcome to the Home Page</h1>
-    <p>This is the content of the home page.</p>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('uploads/idea header image.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('uploads/idea header image.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('uploads/idea header image.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+    </div>
+</div>
+<div>
+    <p>
+        The project “Improving Learning: Developing Measures of Accountability and Evaluating their Association with Student’s Gains in Achievement in Nepal” is awarded to the University of Michigan, USA and the Institute for Social and Environmental Research-Nepal, Nepal funded by the UK Department for International Development (DFID) and the Economic and Social Research Council (ESRC), UK under Award No. ES/L012065/1.
+
+        This three-year project launched on October 1, 2017 is an attempt to develop, pilot test, and conduct research with a set of tools that education officials, community organizations, and education researchers can use to support ongoing efforts toward a well-organized system of social accountability in the Nepalese education system. Specifically, there are two aims of the study:
+
+        To develop and pretest a suite of Nepali Accountability Assessment Tools (NAATs) for use by the Nepal Ministry of Education (MoE) and to pilot these tools within the Chitwan Valley of Nepal.
+        To investigate how accountability processes; environments for student learning in schools, families, and communities; and student learning are related. This involves investigating three main research questions:
+        Are accountability processes systematically related to socioeconomic disparities among communities, schools within communities, and families within schools?
+        In school and community settings where accountability processes are more intensive, is the quality of instructional service delivery higher?
+        After controlling for socioeconomic disparities related to student achievement is student learning higher in schools and communities where accountability processes are more intensive?
+        The project aims to produce six main outputs: (1) production of the NAAT for use by the Nepali government, along with guidance about its use; (2) comprehensive panel data with the potential to answer high priority scientific and policy questions about factors associated with school accountability and student achievement; (3) empirical evidence of the association between community context, school quality, household background, teacher’s background, teaching quality and student achievement, and its interplay with social disparities; (4) improved scientific and analytical capacity of faculty and scientists of the host country institutions; (5) dissemination of the data and the findings at the local, national and global levels; and (6) open access to existing data through ICPSR and the new data through the UK Data Service and publications. The project team hopes that the outputs of this project can be used to support ongoing efforts toward a well-organized system of social accountability in the Nepalese education system.
+    </p>
+</div>
 @endsection

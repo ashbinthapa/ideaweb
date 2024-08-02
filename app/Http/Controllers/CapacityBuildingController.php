1bfe0ae519a9348a98a53f84127bc6f183bfe88f
unknown <ashbinthapa2054@gmail.com>
2024-08-01 20:15:16 -0700

    capacity building desing completed

--- app/Http/Controllers/CapacityBuildingController.php
+++ app/Http/Controllers/CapacityBuildingController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CapacityBuildingController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('capacitybuilding');
+    }
 }
---
+++ resources/views/capacitybuilding.blade.php
@@ -0,0 +1,33 @@
+<!-- resources/views/staff.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'capacity building')
+
+@section('content')
+<div class="ashbin-section">
+    <p class="ashbin-title">Capacity Building</p>
+    <div class="row">
+        <div class="col-3 ashbin-investigator-image">
+            <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser">
+
+        </div>
+        <div class="col-9">
+            <div class="ashbin-investigator-name">
+                PROF. DR. DIRGHA JIBI GHIMIRE
+            </div>
+            <div class="ashbin-investigator-role">
+                PRINCIPAL INVESTIGATOR
+            </div>
+            <div class="ashbin-investigator-phone">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                (734) 763-8649
+            </div>
+            <div class="ashbin-investigator-email">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                nepdjg@umich.edu
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection

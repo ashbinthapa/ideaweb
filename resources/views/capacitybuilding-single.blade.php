be70ed453ed9efb308274855e09c591d71d34e1b
unknown <ashbinthapa2054@gmail.com>
2024-08-03 18:47:58 -0700

    capacity building single page design updated

--- resources/views/capacitybuilding-single.blade.php
+++ resources/views/capacitybuilding-single.blade.php
@@ -5,21 +5,12 @@
 
 @section('content')
 <div class="ashbin-section">
-    <p class="ashbin-title">Capacity Building</p>
+
     <div class="card">
-        <div class="card-header">
-            Featured
-        </div>
         <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
+            <p class="ashbin-title">Capacity Building</p>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <div>
-                <a href="#">
-                    <h3 class="ashbin-button"><span>Read More</span></h3>
-                </a>
-            </div>
         </div>
     </div>
 </div>
-
 @endsection

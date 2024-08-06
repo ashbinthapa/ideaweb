eebf2a215f391c088f597e0879248b6d0f54680a
unknown <ashbinthapa2054@gmail.com>
2024-08-06 13:35:34 -0700

    installed filament and registered admin panel deleted mistake which i have done in registering the panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\AshbinPanelProvider::class,
     App\Providers\HeaderServiveProvider::class,
 ];

1a3838626cd97d5bad71e88a8f42597b7760e57c
unknown <ashbinthapa2054@gmail.com>
2024-09-01 12:06:13 +0545

    gallery page completed

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -9,7 +9,7 @@
     <div class="ashbin-section">
         <div class="card">
             <div class="card-body">
-                <p class="ashbin-title">Capacity Building</p>
+                <p class="ashbin-title">WORKSHOP WITH THE LOCAL GOVERNMENT</p>
                 <div class="row ashbin-gallery-img">
                     <div class="col-lg-4 col-12">
                         <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser">

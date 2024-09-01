b4a51b2a54704d47fbf704010d5d3ebfa59e37e4
unknown <ashbinthapa2054@gmail.com>
2024-09-01 17:56:38 +0545

    contact page model, controller, database, filament-resource created

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact');
+        $posts_all = Contact::all();
+        $data = [
+            'posts_all' => $posts_all
+        ];
+
+        return view('contact', ['data' => $data]);
     }
 }

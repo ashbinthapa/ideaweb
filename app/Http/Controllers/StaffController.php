4a696818d8efc06c43577745f814d4efb195166a
unknown <ashbinthapa2054@gmail.com>
2024-09-09 15:38:24 +0545

    staff model, controller and migtration created

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Staff;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
     public function index()
     {
-        return view('staff');
+        $posts_all = Staff::all();
+        $data = [
+            'posts_all' => $posts_all
+        ];
+
+        return view('staff', ['data' => $data]);
     }
+}
 }

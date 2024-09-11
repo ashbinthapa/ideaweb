5f335d160eaebcd8849672b45afb7e07ff419f08
unknown <ashbinthapa2054@gmail.com>
2024-09-11 15:11:38 +0545

    staff images updated

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -6,28 +6,62 @@
 @section('content')
 <div class="ashbin-section">
     <p class="ashbin-title">Staff</p>
+    @foreach ($data['posts_all'] as $post)
+    @if(($post->michigan) == 0)
     <div class="row">
         <div class="col-3 ashbin-investigator-image">
-            <img src="{{ asset('uploads/idea header image.jpg') }}" alt="Idea-Iser">
-
+            @if(!empty($post->image))
+            <img src="/uploads/{{ $post->image }}" alt="Idea-Iser">
+            @else
+            <img src="/uploads/dummy image.jpg" alt="Idea-Iser">
+            @endif
         </div>
         <div class="col-9">
             <div class="ashbin-investigator-name">
-                PROF. DR. DIRGHA JIBI GHIMIRE
+                {{ $post->name }}
             </div>
             <div class="ashbin-investigator-role">
-                PRINCIPAL INVESTIGATOR
+                {{ $post->position }}
             </div>
-            <div class="ashbin-investigator-phone">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                (734) 763-8649
+            <div class="ashbin-investigator-role">
+                {{ $post->detail }}
             </div>
-            <div class="ashbin-investigator-email">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                nepdjg@umich.edu
+            <div class="ashbin-investigator-role">
+                {{ $post->email }}
             </div>
         </div>
     </div>
+    @endif
+    @endforeach
+
+    @foreach ($data['posts_all'] as $post)
+    @if(($post->michigan) == 1)
+    <p class="ashbin-title">University Of Michigan Staff</p>
+    <div class="row">
+        <div class="col-3 ashbin-investigator-image">
+            @if(!empty($post->image))
+            <img src="/uploads/{{ $post->image }}" alt="Idea-Iser">
+            @else
+            <img src="/uploads/dummy image.jpg" alt="Idea-Iser">
+            @endif
+        </div>
+        <div class="col-9">
+            <div class="ashbin-investigator-name">
+                {{ $post->name }}
+            </div>
+            <div class="ashbin-investigator-role">
+                {{ $post->position }}
+            </div>
+            <div class="ashbin-investigator-role">
+                {{ $post->detail }}
+            </div>
+            <div class="ashbin-investigator-role">
+                {{ $post->email }}
+            </div>
+        </div>
+    </div>
+    @endif
+    @endforeach
 </div>
 
 @endsection

c9b522acae0405c987481616e62a09e1cb467e9f
unknown <ashbinthapa2054@gmail.com>
2024-09-12 11:48:29 +0545

    capacity-building and activity single page updated

--- resources/views/capacitybuilding.blade.php
+++ resources/views/capacitybuilding.blade.php
@@ -23,7 +23,7 @@
             </p>
             <p>{{ Str::limit(strip_tags($post->content), 250) }}</p>
             <div>
-                <a href="{{ $post->slug }}">
+                <a href="capacitybuilding/{{ $post->slug }}">
                     <h3 class="ashbin-button"><span>Read More</span></h3>
                 </a>
             </div>

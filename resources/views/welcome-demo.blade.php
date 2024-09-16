c71ae568f360a13f517695cb4f7d5b796d0a185b
unknown <ashbinthapa2054@gmail.com>
2024-09-16 15:46:59 +0545

    vendor folder removed from git ignore

--- resources/views/welcome-demo.blade.php
+++ resources/views/welcome-demo.blade.php
@@ -1,12 +1,14 @@
 <!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
     <header>
         @include('partials.header')
@@ -18,4 +20,5 @@
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
